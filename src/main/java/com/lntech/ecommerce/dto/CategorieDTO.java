922f65c60ed220616c591080b9386d32e7cc4f65
Luan Reis <luanreis2202@gmail.com>
2022-03-04 00:36:12 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/lntech/ecommerce/dto/CategorieDTO.java
+++ src/main/java/com/lntech/ecommerce/dto/CategorieDTO.java
@@ -1,13 +1,18 @@
-package com.lntech.ecommerce.dto;
+package com.lntech.ecommerce.domain.dto;
 
 import com.lntech.ecommerce.domain.Categorie;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
 public class CategorieDTO  implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private Integer id;
+
+    @NotEmpty(message = "Preenchimento obrigatório")
+    @Length(min = 3,max = 80,message = "O tamanho deve ter entre 3 e 80 caracteres.")
     private String name;
 
     public CategorieDTO() {
