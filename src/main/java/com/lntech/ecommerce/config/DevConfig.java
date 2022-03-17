82823277afe696d0471d0bc9288ac66f3430810b
Luan Reis <luanreis2202@gmail.com>
2022-03-17 10:17:48 -0300

    Criando o profile de desenvolvimento

---
+++ src/main/java/com/lntech/ecommerce/config/DevConfig.java
@@ -0,0 +1,30 @@
+package com.lntech.ecommerce.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Bean
+    public boolean instantiateDatabase() throws ParseException {
+
+        if(!"create".equals(strategy)){
+            return false;
+        }
+        dbService.instantiateTestDatabase();
+        return true;
+    }
+}
